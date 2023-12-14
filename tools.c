3ea57bddaeea29c77d8da36d888105457aa4dffd
Yohannes Guesh <yohannesguesh01@gmail.com>
2023-12-14 05:38:37 -0800

    updated

--- tools.c
+++ tools.c
@@ -92,14 +92,6 @@ void find_func(char *opcode, char *value, int ln, int format)
 		{"nop", nop},
 		{"swap", swap_nodes},
 		{"add", add_nodes},
-		{"sub", sub_nodes},
-		{"div", div_nodes},
-		{"mul", mul_nodes},
-		{"mod", mod_nodes},
-		{"pchar", print_char},
-		{"pstr", print_str},
-		{"rotl", rotl},
-		{"rotr", rotr},
 		{NULL, NULL}
 	};
 
