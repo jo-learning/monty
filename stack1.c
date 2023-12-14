877a6c11a00645a362f989c421f9835ec08fe018
Yohannes Guesh <yohannesguesh01@gmail.com>
2023-12-14 05:45:00 -0800

    updated

--- stack1.c
+++ stack1.c
@@ -4,7 +4,7 @@
 /**
  * add_to_stack - Adds a node to the stack.
  * @new_node: Pointer to the new node.
- * @ln: Interger representing the line number of of the opcode.
+ * @line: Interger representing the line number of of the opcode.
  */
 void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int line)
 {
